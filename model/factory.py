b262c5860403cf91d79cf3f367faf3b5e82a08ec
Cat-ddd <344968424@qq.com>
2025-09-18 11:30:54 +0800

    Update factory.py

--- model/factory.py
+++ model/factory.py
@@ -1,57 +1,54 @@
-# model/factory.py
-
 from .rf import RandomForestModel
 from .svm import SVMModel
-from .logistic import LogisticRegressionModel
-from .linear import LinearRegressionModel
 from .xgb import XGBoostModel
 from .transformer import TransformerModel
 from .lstm import LSTMModel
 from .esm_model import ESMModel
 from .pepbert import PepBERTModel
 
-def build_model(name, task, input_dim=None, max_len=50, device=None, model_path=None):
+def build_model(name, task, input_dim=None, max_len=50, device=None, model_path=None, random_state=42, **kwargs):
     """
     构建模型对象。
 
     参数:
         name: 模型名称（如 'rf', 'svm', 'transformer' 等）
         task: 'classification' 或 'regression'
-        input_dim: 对于深度学习模型，输入维度（如 one-hot 后维度）
-        max_len: 输入序列最大长度，仅用于 Transformer
+        input_dim: 每个输入样本的特征向量
+        max_len: 输入序列最大长度
         device: 用于深度模型的计算设备（如 'cuda' 或 'cpu'）
+        random_state: 随机种子，用于传统模型和接口统一
 
     返回:
         已实例化的模型对象
     """
     name = name.lower()
 
+    if model_path is None:
+        if name == "pepbert":
+            model_path = "MODEL/prot_bert"
+        elif name == "esm":
+            model_path = "MODEL/esm2_t12_35M_UR50D"
+
     if name == 'rf':
-        return RandomForestModel(task=task)
+        return RandomForestModel(task=task, random_state=random_state, **kwargs)
 
     elif name == 'svm':
-        return SVMModel(task=task)
-
-    elif name == 'logistic':
-        return LogisticRegressionModel(task=task)
-
-    elif name == 'linear':
-        return LinearRegressionModel(task=task)
+        return SVMModel(task=task, random_state=random_state, **kwargs)
 
     elif name == 'xgb':
-        return XGBoostModel(task=task)
+        return XGBoostModel(task=task, random_state=random_state, **kwargs)
 
     elif name == 'transformer':
-        return TransformerModel(input_dim=input_dim, max_len=max_len, task=task, device=device)
+        return TransformerModel(task=task, device = device, max_len=max_len)
     
     elif name == 'lstm':
-        return LSTMModel(input_dim=input_dim, task=task, device=device)
+        return LSTMModel(task=task, device=device, max_len = max_len)
 
-    elif name == 'esm':
-        return ESMModel(model_path=model_path, task=task, device=device)
+    elif name == "pepbert":
+        return PepBERTModel(model_path=model_path, device=device, max_len=max_len)
 
-    elif name == 'pepbert':
-        return PepBERTModel(model_path=model_path, task=task, device=device)
+    elif name == "esm":
+        return ESMModel(model_path=model_path, device=device, max_len=max_len)
 
     else:
         raise ValueError(f"Unsupported model name: {name}")
