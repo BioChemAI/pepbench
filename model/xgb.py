7838ee074ca0ecf742ab7ac558a3c701f092f2c3
Cat-ddd <344968424@qq.com>
2025-10-29 10:41:10 +0800

    Refactor XGBoostModel to use multi-threading

--- model/xgb.py
+++ model/xgb.py
@@ -1,8 +1,11 @@
+# model/xgb.py
 from xgboost import XGBClassifier, XGBRegressor
 from .base import BaseModel
 
 class XGBoostModel(BaseModel):
-    def __init__(self, task='classification', random_state=42, **kwargs):
+    def __init__(self, task='classification',
+                 random_state=42,
+                 **kwargs):
         super().__init__()
         self.task = task
         if task == 'classification':
@@ -10,27 +13,29 @@ class XGBoostModel(BaseModel):
                 use_label_encoder=False,
                 eval_metric='logloss',
                 random_state=random_state,
-                n_jobs=1,  # 控制多线程以减少随机性
+                n_jobs=-1,
                 **kwargs
             )
         elif task == 'regression':
             self.model = XGBRegressor(
                 random_state=random_state,
-                n_jobs=1,
+                n_jobs=-1,
                 **kwargs
             )
         else:
-            raise ValueError("Task must be either 'classification' or 'regression'.")
+            raise ValueError("--Task must be either 'classification' or 'regression'.--")
 
+    # define the training method
     def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
 
+    # define the prediction method
     def predict(self, X):
         if self.task == "classification":
-            return self.model.predict_proba(X)[:, 1]  # 概率，用于计算 AUC 等
+            return self.model.predict_proba(X)[:, 1]
         else:  # regression
-            return self.model.predict(X)  # 连续值
+            return self.model.predict(X)
 
-
+    # define the evaluation method
     def evaluate(self, X, y):
-        return self.model.score(X, y)  # 分类：accuracy，回归：R²
+        return self.model.score(X, y)  # classification-> accuracy，regression-> R²
