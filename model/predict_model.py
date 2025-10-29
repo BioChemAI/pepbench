8ac6047cea16b54ae7e5bd0c15900fed1f5c34db
Cat-ddd <344968424@qq.com>
2025-10-29 10:49:18 +0800

    Implement PredictModel class for classification tasks

---
+++ model/predict_model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class PredictModel(nn.Module):
+    def __init__(self,hidden_size, task):
+        super().__init__()
+        if task == 'classification':
+            self.output_layer = nn.Sequential(
+                nn.Linear(hidden_size, 128),
+                nn.ReLU(),
+                nn.Linear(128, 32),
+                nn.ReLU(),
+                nn.Linear(32, 1),
+                nn.Sigmoid()
+            )
+        else:
+            self.output_layer = nn.Sequential(
+                nn.Linear(hidden_size, 128),
+                nn.ReLU(),
+                nn.Linear(128, 32),
+                nn.ReLU(),
+                nn.Linear(32, 1)
+            )
+    def forward(self,embedding):
+        out = self.output_layer(embedding)
+        return out
