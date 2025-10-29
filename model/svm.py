e2220884a38c2dee95b90d9d9daf849b180982d0
Cat-ddd <344968424@qq.com>
2025-10-29 10:40:05 +0800

    Enhance SVMModel to accept additional kwargs
    Added support for additional keyword arguments in SVMModel initialization.

--- model/svm.py
+++ model/svm.py
@@ -1,30 +1,35 @@
-# svm.py
+# model/svm.py
 from sklearn.svm import SVC, SVR
 from .base import BaseModel
 
 class SVMModel(BaseModel):
-    def __init__(self, task='classification', C=1.0, kernel='rbf', random_state=42):
+    def __init__(self, task='classification',
+                 C=1.0,
+                 kernel='rbf',
+                 random_state=42,
+                 **kwargs):
         super().__init__()
         self.task = task
 
         if task == 'classification':
+            # output probability: probability=True
             self.model = SVC(C=C, kernel=kernel, probability=True, random_state=random_state)
         elif task == 'regression':
-            self.model = SVR(C=C, kernel=kernel)
+            self.model = SVR(C=C, kernel=kernel, random_state=random_state)
         else:
-            raise ValueError("Task must be 'classification' or 'regression'")
+            raise ValueError("--Task must be 'classification' or 'regression'.--")
 
+    # define the training method
     def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
 
+    # define the prediction method
     def predict(self, X):
-        # 分类返回概率，回归返回预测值
         if self.task == 'classification':
-            return self.model.predict_proba(X)[:, 1]  # 返回正类概率
+            return self.model.predict_proba(X)[:, 1]  # probability
         else:
             return self.model.predict(X)
 
+    # define the evaluation method
     def evaluate(self, X, y):
-        # 分类用准确率，回归用 R^2
-        return self.model.score(X, y)
-
+        return self.model.score(X, y)  # classification-> accuracy，regression-> R²
