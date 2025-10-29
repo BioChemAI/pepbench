fd0dd50c7ccd9c385fea502f7cfbe56dda3b9463
Cat-ddd <344968424@qq.com>
2025-10-29 10:30:40 +0800

    Refactor OneHotEncoder for batch processing

--- feature/OneHot.py
+++ feature/OneHot.py
@@ -1,47 +1,35 @@
-
-# import numpy as np
-
-# class OneHotEncoder:
-#     def __init__(self, max_len=50, flatten=True):
-#         self.amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
-#         self.aa_to_idx = {aa: idx for idx, aa in enumerate(self.amino_acids)}
-#         self.unknown_index = 20  # 第21类（未知氨基酸）
-#         self.max_len = max_len   # 固定序列长度
-#         self.flatten = flatten   # 是否展开为一维向量
-
-#     def encode(self, sequence):
-#         sequence = sequence[:self.max_len]  # 截断过长序列
-#         encoding = np.zeros((self.max_len, 21), dtype=np.float32)
-#         for i, aa in enumerate(sequence):
-#             idx = self.aa_to_idx.get(aa, self.unknown_index)
-#             encoding[i][idx] = 1.0
-
-#         if self.flatten:
-#             return encoding.flatten()  # shape: (max_len × 21,)
-#         else:
-#             return encoding            # shape: (max_len, 21)
-
-
+'''
+fun:aas--->[0,1,0,...,0]s
+dim:max_len*20, numpy array
+'''
 import numpy as np
-
 class OneHotEncoder:
     def __init__(self, max_len=50, flatten=True):
-        self.amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
-        self.aa_to_idx = {aa: idx for idx, aa in enumerate(self.amino_acids)}
         self.max_len = max_len
         self.flatten = flatten
 
-    def encode(self, sequence):
-        encoding = np.zeros((self.max_len, 20), dtype=np.float32)
+        self.aas = 'ACDEFGHIKLMNPQRSTVWY'
+        self.dim = 20  # one-hot encode dim:20/21
+        self.aa_to_idx = {aa: idx for idx, aa in enumerate(self.aas)}
 
-        for i in range(min(len(sequence), self.max_len)):
-            aa = sequence[i]
-            if aa in self.aa_to_idx:
-                idx = self.aa_to_idx[aa]
-                encoding[i][idx] = 1.0
-            # 如果未知氨基酸，整行为0，可跳过
+    def encode(self, sequences):
+        # sequence = sequence.upper()
+        n_samples = len(sequences)
+        encoded_list = []
+        
+        for seq in sequences:
+            # (max_len,20) for a sequence
+            encoded_vector = np.zeros((self.max_len, self.dim), dtype=np.float32)
+            for i in range(min(len(seq), self.max_len)):
+                aa = seq[i]
+                if aa in self.aa_to_idx:
+                    idx = self.aa_to_idx[aa]
+                    encoded_vector[i][idx] = 1.0
+            encoded_list.append(encoded_vector)
+
+        batch_encoded = np.stack(encoded_list)           # shape: (n_samples, max_len, 20)
 
         if self.flatten:
-            return encoding.flatten()  # shape: (max_len * 20,)
+            return batch_encoded.reshape(n_samples, -1)  # shape: (n_samples, max_len * 20,)
         else:
-            return encoding            # shape: (max_len, 20)
+            return batch_encoded                         # shape: (n_samples, max_len, 20)
